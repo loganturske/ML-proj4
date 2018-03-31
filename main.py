e53d08a77024f7315f90a227bcda2bc321c80dff
loganturske <loganturske@gmail.com>
2018-03-31 14:59:47 -0400

    comments more

--- main.py
+++ main.py
@@ -299,121 +299,212 @@ def build_tree(data, attributes, target):
 	# Return the tree
 	return Node(best, values, tree, default, 1)
 
+
+#
+# This function will recurse down the tree and once it reaches a leaf node determine
+# if it got the correct answer
+#
 def recurse_tree(tree, test_entry, attributes):
-
+	# If you have no children you are a leaf
 	if not tree.children:
+		# If the class of the leaf matches the test
 		if test_entry[-1] == tree.default:
+			# Return a 1 which is true
 			return 1
+		# Other wise you are incorrect
 		else:
+			# Return a 0 which is incorrect
 			return 0
+	# Get the index of the attribute you are about to recurse on
 	index = attributes.index(tree.attr)
+	# Get the value of the attribute of the test entry
 	value = test_entry[index]
-	i = 0
+	# Set a counter
+	count = 0
+	# For each value in the tress values
 	for val in tree.values:
+		# If you found the corresponding value to the test entry
 		if value == val:
-			return recurse_tree(tree.children[i], test_entry, attributes)
-		i += 1
+			# Recurse down
+			return recurse_tree(tree.children[count], test_entry, attributes)
+		# Increment counter
+		count += 1
 
+
+#
+# This function will determine if you can still prune the tree
+#
 def can_be_pruned(tree):
-	r = False
+	# Set a value to be returned
+	ret = False
+	# If the tree you are on has a 1 for prune
 	if tree.prune == 1:
-		r =  True
+		# Set the return value to be true
+		ret =  True
+	# If you have children
 	if tree.children != None:
+		# For each subtree in tree
 		for subtree in tree.children:
+			# Recurse down and see if they can be pruned, if so
 			if can_be_pruned(subtree) == True:
-				r = True
-	return r
+				# Set return value to be true
+				ret = True
+	# Return the value
+	return ret
 
-# done = 1 if you pruned already
+#
+# This function will prune the tree, it will only prune one node
+#
 def prune_tree(tree):
-
+	# If you you found a tree is marked to be pruned
 	if tree.prune == 1:
+		# Set it to not be pruned again
 		tree.prune = 0
+		# Remove all of your children
 		tree.children = None
+		# Return the tree and a 1 to say you pruned already
 		return (tree, 1)
+	# Set a vairbale to check if you are done pruning
 	done = 0
+	# If you have children
 	if tree.children != None:
-		i = 0
+		# Set a counter
+		count = 0
+		# For each of the trees subtrees
 		for subtree in tree.children:
+			# Recurse down and get the values
 			ret = prune_tree(subtree)
-			tree.children[i] = ret[0]
+			# Set your subtree to get the one returned
+			tree.children[count] = ret[0]
+			# If you got a 1 saying you are done pruning
 			if ret[1] == 1:
+				# Set done to 1
 				done = 1
+				# Break out of the loop to stop recursing
 				break
-			i += 1
+			# Increment counter by 1
+			count += 1
+	# Return yourself and the done variabel
 	return (tree, done)
 
-already_marked = 0
+
+#
+# This function will mark the node of a tree to be pruned next, to 0
+# this is used for when you need to just say that you do not need to prune that node
+#
 def mark_node(tree):
+	# If you found the tree that needs to be pruned
 	if tree.prune == 1:
+		# Set prune of the tree to be 0
 		tree.prune = 0
+		# Return yourself and 1 to say that you are done
 		return (tree, 1)
+	# Set a variable that will determine if you are done looking for the tree to mark
 	done = 0
+	# If you have children
 	if tree.children != None:
-		i = 0
+		# Set a counter
+		count = 0
+		# For each subtree of the tree
 		for subtree in tree.children:
+			# Recurse down and get the variables
 			ret = prune_tree(subtree)
-			tree.children[i] = ret[0]
+			# Set your child to the one returnd 
+			tree.children[count] = ret[0]
+			# If you got back a 1 to say you are done looking
 			if ret[1] == 1:
+				# Set the done variable to be 1
 				done = 1
+				# Break out of the loop
 				break
-			i += 1
+			# Increment counter by 1
+			count += 1
+	# Return yourself and the done variable
 	return (tree, done)
-# This function runs the decision tree algorithm. It parses the file for the data-set, and then it runs the 10-fold cross-validation. It also classifies a test-instance and later compute the average accurracy
-# Improvements Used: 
-# 1. Discrete Splitting for attributes "age" and "fnlwght"
-# 2. Random-ness: Random Shuffle of the data before Cross-Validation
+
+
+#
+# This function will run a decisionon tree on the data set and attributes passed in.
+# You can also set if you want to run reduced error pruning
 def run_decision_tree(data, attributes, prune):
-
+	# Get all of the classes for the dataset
 	target = attributes[-1]
-
+	# Cross fold validation K
 	K = 5
+	# Set a vairbale for the overall accuracy
 	acc = []
+	# For each fold of the cross fold validation
 	for k in range(K):
+		# Randomize the data
 		random.shuffle(data)
+		# Get the training set of the fold
 		training_set = [x for i, x in enumerate(data) if i % K != k]
+		# Get the validation set of the fold
 		validation_set = [x for i, x in enumerate(data) if i % K == k]
+		# Create a tree
 		tree = DecisionTree()
-		# Grow the tree
+		# Grow the tree to completion based on the training set
 		tree.learn( training_set, attributes, target )
 
-		# results = []
-		# for test_entry in validation_set:
-		# 	answer = recurse_tree(tree.tree, test_entry, attributes)
-		# 	if answer != None:
-		# 		results.append(answer)
-		# accuracy = float(results.count(1))/float(len(results))
-		# print accuracy
-
-		saved = copy.deepcopy(tree.tree)
-		p = can_be_pruned(saved)
-		print "START"
-		while(p):
-			temp = copy.deepcopy(saved)
+		if (prune):
 			results = []
 			for test_entry in validation_set:
-				answer = recurse_tree(temp, test_entry, attributes)
+				answer = recurse_tree(tree.tree, test_entry, attributes)
 				if answer != None:
 					results.append(answer)
-			old_accuracy = float(results.count(1))/float(len(results))
-			prune_tree(temp)
+			accuracy = float(results.count(1))/float(len(results))
+			print accuracy
+		else:
+			# Save a copy of the tree off
+			saved = copy.deepcopy(tree.tree)
+			# See if the tree can be pruned
+			can_prune = can_be_pruned(saved)
 
-			new_results = []
-			for test_entry in validation_set:
-				answer = recurse_tree(temp, test_entry, attributes)
-				if answer != None:
-					new_results.append(answer)
-			new_accuracy = float(new_results.count(1))/float(len(new_results))
+			print "START"
+			# While you are able to prune the tree
+			while(can_prune):
+				# Save a copy of the tree so far
+				temp = copy.deepcopy(saved)
+				# Set a variable to keep track of the results
+				results = []
+				# For each test entry in the validation set
+				for test_entry in validation_set:
+					# See if you can classify the test entry 
+					answer = recurse_tree(temp, test_entry, attributes)
+					# If you got an answer right or wrong
+					if answer != None:
+						# Append it to the results list
+						results.append(answer)
+				# Calculate the accruacy of the tree against the validation set
+				old_accuracy = float(results.count(1))/float(len(results))
+				# Prune the tree
+				prune_tree(temp)
+				# Set a new results list
+				new_results = []
+				# For each test entry in the validation set
+				for test_entry in validation_set:
+					# See if you can classify the test entry
+					answer = recurse_tree(temp, test_entry, attributes)
+					# If you got an anser right or wrong
+					if answer != None:
+						# Add the answer to the results list
+						new_results.append(answer)
+				# Calculate the accuracy of the pruned tree
+				new_accuracy = float(new_results.count(1))/float(len(new_results))
 
-			# print str(old_accuracy) + " VS " + str(new_accuracy)
+				# print str(old_accuracy) + " VS " + str(new_accuracy)
 
-			if old_accuracy < new_accuracy:
-				saved = copy.deepcopy(temp)
-			else:
-				# print "NO"
-				mark_node(saved)
-			p = can_be_pruned(saved)
-		print "END"
+				# If you increased accuracy pruning the tree
+				if old_accuracy < new_accuracy:
+					# The saved copy of the tree is now the pruned tree
+					saved = copy.deepcopy(temp)
+				# If you did not increase the accuracy by pruning
+				else:
+					# Mark the node to not be pruned again
+					mark_node(saved)
+				# See if you can prune the tree still
+				can_prune = can_be_pruned(saved)
+			print "END"
 	# 	acc.append(accuracy)
 
 	# avg_acc = sum(acc)/len(acc)
